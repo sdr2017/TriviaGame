27735af00b92abb5711f3f528886348e3ac90d07
sdr2017 <sarahjeanlinnebur@Sarah-Linneburs-MacBook-Pro.local>
2017-05-31 00:30:17 -0600

    beginning pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//I need an array of questions with answers
+//I need to randomly select 5 questions from that array and display them in the HTML
+//I need to specify the correct answer in the HTML value? (give it a value of true?)
+
+//I need set a countdown timer
+
+//I need user to select only one answer per question
+
+//If time runs out OR user hits "done"
+	//I need to pull the information from HTML:
+		//how many correct
+		//how many false
+		//how many unanswered
