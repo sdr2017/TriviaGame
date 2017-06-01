58f682f3599eed35cbdf45f81be191f330e461a2
sdr2017 <sarahjeanlinnebur@Sarah-Linneburs-MacBook-Pro.local>
2017-05-31 22:03:12 -0600

    starting to build question objects/arrays and working on randomization to send to HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,3 +11,34 @@
 		//how many correct
 		//how many false
 		//how many unanswered
+//I need to print that information on a new HTML page
+//After XX seconds, I need to go back to the gamepage and reset the game
+$(document).ready(function() {
+	var trivia = [{
+			//trivia[0]
+			question: "What do you call a group of Owls?",
+			answer: "a parliament",
+			options: ["a congress", "a ministry", "a group"];
+	},{		//trivia[1]
+			question: "What do you call a group of Penguins?",
+			answer: "All of the Above",
+			option1: "a colony",
+			option2: "a muster",
+			option3: "a parcel",
+	},{		//trivia[2]
+			question: "What do you call a group of Sparrows?",
+			answer: "a host",
+			option1: "a cluster",
+			option2: "an army",
+			option3: "a legion",
+	}]
+
+	var triviaRandom = trivia[Math.floor(Math.random() * trivia.length)];
+	console.log(triviaRandom);
+
+	$("#question1").html(trivia[0].question);
+
+
+
+})
+
