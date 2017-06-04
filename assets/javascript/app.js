8636cc8995acba009db4c94748cbb29e09a962bc
sdr2017 <sarahjeanlinnebur@Sarah-Linneburs-MacBook-Pro.local>
2017-06-04 11:14:30 -0600

    attempting to shuffle options, adding answer to options array in trivia

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -41,77 +41,77 @@ $(document).ready(function() {
 			//trivia[0]
 			question: "What do you call a group of Owls?",
 			answer: "a parliament",
-			options: ["a congress", "a ministry", "a group"],
+			options: ["a congress", "a ministry", "a group", "a parliament"],
 			asked: false,
 	},{		//trivia[1]
 			question: "What do you call a group of Penguins?",
 			answer: "All of the Above",
-			options: ["a colony", "a muster", "a parcel"],
+			options: ["a colony", "a muster", "a parcel", "All of the Above"],
 			asked: false,
 	},{		//trivia[2]
 			question: "What do you call a group of Sparrows?",
 			answer: "a host",
-			options: ["a cluster", "an army", "a legion"],
+			options: ["a cluster", "an army", "a legion", "a host"],
 			asked: false,
 	},{		//trivia[3]
 			question: "What do you call a group of Flamingos?",
 			answer: "a stand",
-			options: ["a garden", "crowd", "school"],
+			options: ["a garden", "crowd", "school", "a stand"],
 			asked: false,
 	},{		//trivia[4]
 			question: "What do you call a group of Grasshoppers?",
 			answer: "a cloud",
-			options: ["a ninja", "a swarm", "a cricket"],
+			options: ["a ninja", "a swarm", "a cricket", "a cloud"],
 			asked: false,
 	},{		//trivia[5]
 			question: "About how many species are of snakes are there?",
 			answer: "3000",
-			options: ["100", "300", "1000"],
+			options: ["100", "300", "1000", "3000"],
 			asked: false,
 	},{		//trivia[6]
 			question: "What is considered to be the smartest breed of dog?",
 			answer: "Border Collie",
-			options: ["Labrador Retreiver", "Clifford", "Pit Bull"],
+			options: ["Labrador Retreiver", "Clifford", "Pit Bull", "Border Collie"],
 			asked: false,
 	},{		//trivia[7]
 			question: "What method do bats use for navigation?",
 			answer: "Echolocation",
-			options: ["GPS", "Wingardium Leviosa", "Maps"],
+			options: ["GPS", "Wingardium Leviosa", "Maps", "Echolocation"],
 			asked: false,
 	},{		//trivia[8]
 			question: "Which mammal lays eggs?",
 			answer: "Platypus",
-			options: ["Kangaroo", "Dolphin", "Chicken"],
+			options: ["Kangaroo", "Dolphin", "Chicken", "Platypus"],
 			asked: false,
 	},{		//trivia[9]
 			question: "In what year did the passenger pigeon go extinct?",
 			answer: "1914",
-			options: ["1080", "1492", "2012"],
+			options: ["1080", "1492", "2012", "1914"],
 			asked: false,
 	},{		//trivia[10]
 			question: "What two species breed to create a mule?",
 			answer: "Donkey and Horse",
-			options: ["Zebra and Horse", "Mule and Mule", "Cow and Horse"],
+			options: ["Zebra and Horse", "Mule and Mule", "Cow and Horse", "Donkey and Horse"],
 			asked: false,
 	},{		//trivia[11]
 			question: "What animal was considered sacred in Ancient Egypt?",
 			answer: "Cat",
-			options: ["Mouse", "Turtle", "Lady bug"],
+			options: ["Mouse", "Turtle", "Lady bug", "Cat"],
 			asked: false,
 	},{		//trivia[12]
 			question: "In what region do Narwhals live?",
 			answer: "Arctic",
-			options: ["South Pacific", "South Atlantic", "Saharan"],
+			options: ["South Pacific", "South Atlantic", "Saharan", "Arctic",],
 			asked: false,
 	},{		//trivia[13]
 			question: "What type of bird did Darwin observe, resulting in his Theory of Evolution?",
 			answer: "Finches",
-			options: ["Eagles", "Falcons", "Ravens"],
+			options: ["Eagles", "Falcons", "Ravens", "Finches"],
 			asked: false,
 	},{		//trivia[14]
 			question: "Which sea animal has three hearts?",
 			answer: "Cuttlefish",
-			options: ["Starfish", "Sharks", "Dolphin"],
+			options: ["Starfish", "Sharks", "Dolphin", "Cuttlefish"],
 			asked: false,
 	}];
 
@@ -183,52 +183,39 @@ function resetGame() {
 	// 	five: [".question5a", ".question5b", ".question5c", ".question5d"],
 	// }]
 
-	// function options() {
-// 	var totalOptions0 = [triviaToPrint[0].options + "," + triviaToPrint[0].answer];
-// 	var splitOptions0 = totalOptions0.split(",");
-// 	var totalOptions1 = [(triviaToPrint[1].options + triviaToPrint[1].answer)];
-// 	var totalOptions2 = [(triviaToPrint[2].options + triviaToPrint[2].answer)];
-// 	var totalOptions3 = [(triviaToPrint[3].options + triviaToPrint[3].answer)];
-// 	var totalOptions4 = [(triviaToPrint[4].options + triviaToPrint[4].answer)];
-// 	console.log(splitOptions0);
-// }
-// options();
-	
 	for (var i = 0; i < totalQuestions; i++) {
 		var triviaRandom = trivia[Math.floor(Math.random() * trivia.length)];//randomly selecting questions(amount determined in HTML)
 		triviaToPrint.push(triviaRandom);//pushing randomly selected questions to array
-	 }
+		}
 
 	//printing random questions to HTML
 	for (var i = 0; i <displayQuestions.length; i++) {
 	 	$(displayQuestions[i]).html(triviaToPrint[i].question);
 		}
 
-		$(".question1a").html('<label><input type="radio" name="question1"></label>' + triviaToPrint[0].options[0]);
-		$(".question1b").html('<label><input type="radio" name="question1"></label>' + triviaToPrint[0].options[1]);
-		$(".question1c").html('<label><input type="radio" name="question1"></label>' + triviaToPrint[0].answer);
-		$(".question1d").html('<label><input type="radio" name="question1"></label>' + triviaToPrint[0].options[2]);
+		function shuffle(options) {
+			var index = options.length;
+			var	temporaryValue;
+			var	randomIndex;
+			while (0 !== index) {
+				randomIndex = Math.floor(Math.random() * index);
+				index -= 1;
 
-		$(".question2a").html('<label><input type="radio" name="question2"></label>' + triviaToPrint[1].answer);
-		$(".question2b").html('<label><input type="radio" name="question2"></label>' + triviaToPrint[1].options[1]);
-		$(".question2c").html('<label><input type="radio" name="question2"></label>' + triviaToPrint[1].options[0]);
-		$(".question2d").html('<label><input type="radio" name="question2"></label>' + triviaToPrint[1].options[2]);
+				temporaryValue = options[index];
+				options[index] = options[randomIndex];
+				options[randomIndex] = temporaryValue;
+			}
+			return options;
+		}
 
-		$(".question3a").html('<label><input type="radio" name="question3"></label>' + triviaToPrint[2].options[1]);
-		$(".question3b").html('<label><input type="radio" name="question3"></label>' + triviaToPrint[2].answer);
-		$(".question3c").html('<label><input type="radio" name="question3"></label>' + triviaToPrint[2].options[0]);
-		$(".question3d").html('<label><input type="radio" name="question3"></label>' + triviaToPrint[2].options[2]);
+		for (var i = 0; i < displayQuestions.length; i++) {
+			triviaToPrint[i].options = shuffle(triviaToPrint[i].options);
+			console.log(triviaToPrint[i].options);
+		}
+ }
 
-		$(".question4a").html('<label><input type="radio" name="question4"></label>' + triviaToPrint[3].options[2]);
-		$(".question4b").html('<label><input type="radio" name="question4"></label>' + triviaToPrint[3].options[0]);
-		$(".question4c").html('<label><input type="radio" name="question4"></label>' + triviaToPrint[3].options[1]);
-		$(".question4d").html('<label><input type="radio" name="question4"></label>' + triviaToPrint[3].answer);
 
-		$(".question5a").html('<label><input type="radio" name="question5"></label>' + triviaToPrint[4].options[2]);
-		$(".question5b").html('<label><input type="radio" name="question5"></label>' + triviaToPrint[4].answer);
-		$(".question5c").html('<label><input type="radio" name="question5"></label>' + triviaToPrint[4].options[1]);
-		$(".question5d").html('<label><input type="radio" name="question5"></label>' + triviaToPrint[4].options[0]);
-
+	
 var checked;
 var correct;
 var wrong;
@@ -238,6 +225,6 @@ var wrong;
 
 
 
-}
+
 })
 
